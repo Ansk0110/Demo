af9592fde0b4bf01cba618ad61bea3cae1be1b5a
Ansk0110 <abhi.ravi.sai@gmail.com>
2018-05-31 14:09:30 +0530

    /Fire base login is set up with a slash screen that will choose to login or sign in directly

---
+++ app/src/main/java/com/example/omniwyse/myapplication/ChatApp/ChatAppSignIn.kt
@@ -0,0 +1,65 @@
+package com.example.omniwyse.myapplication.ChatApp
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.omniwyse.myapplication.R
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import kotlinx.android.synthetic.main.activity_chat_app_sign_in.*
+import org.jetbrains.anko.clearTask
+import org.jetbrains.anko.design.longSnackbar
+import org.jetbrains.anko.indeterminateProgressDialog
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
+
+class ChatAppSignIn : AppCompatActivity() {
+
+    private  val Rc_signIn = 1
+
+    private  val signInProviders =
+            listOf(AuthUI.IdpConfig.EmailBuilder()
+                    .setAllowNewAccounts(true)
+                    .setRequireName(true)
+                    .build())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_app_sign_in)
+
+        account_sign_in.setOnClickListener {
+            val intent = AuthUI.getInstance().createSignInIntentBuilder()
+                    .setAvailableProviders(signInProviders)
+                    .setLogo(R.drawable.ic_chat)
+                    .build()
+
+            startActivityForResult(intent,Rc_signIn)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == Rc_signIn){
+            val response = IdpResponse.fromResultIntent(data)
+
+            if(resultCode == Activity.RESULT_OK){
+                val progressDailog = indeterminateProgressDialog("Setting up your account")
+                startActivity(intentFor<ChatActivity>().newTask().clearTask())
+                progressDailog.dismiss()
+            }
+            else if (resultCode == Activity.RESULT_CANCELED){
+                if (response == null) return
+
+                when(response.error?.errorCode){
+                    ErrorCodes.NO_NETWORK ->
+                            longSnackbar(signin_layout,"No network")
+                    ErrorCodes.UNKNOWN_ERROR ->
+                        longSnackbar(signin_layout,"Unknown Error")
+                }
+            }
+        }
+    }
+}
